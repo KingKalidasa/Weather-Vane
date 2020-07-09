3a60195440ed15cf42e508bbf2d4009012ff6030
KingKalidasa <65272630+KingKalidasa@users.noreply.github.com>
2020-07-09 00:01:48 -0500

    Delete keys.js

--- keys.js
+++
@@ -1,4 +0,0 @@
-const mapboxKey = "pk.eyJ1IjoibHF1aW5vbmVzIiwiYSI6ImNrYXZvdWNsejExYWoycm9obnVieHM1YTgifQ.0pG_h0h4ZXd0gmDeheA16g";
-const weatherStackKey = "d7a1dabe9e685330d2bf9946c495e37d";
-
-module.exports = {mapboxKey, weatherStackKey};
